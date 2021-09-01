b5fcf652fecc16337885e6cfe802170074b55236
Cool-Vandit <73426806+Cool-Vandit@users.noreply.github.com>
2021-09-01 11:53:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+status = "";
 function preload(){
 
 }
@@ -5,4 +6,21 @@ function preload(){
 function setup(){
     canvas = createCanvas(430,300);
     canvas.center();
+    video = createCapture(VIDEO);
+    vide0.hide();
+    video.size(430,300);
+}
+
+function draw(){
+    image(video,0,0,430,300);
+}
+
+function start() {
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    value  = document.getElementById("status").value;
+}
+
+function modelLoaded(){
+ console.log("model is loaded ");
+ status = true ; 
 }
